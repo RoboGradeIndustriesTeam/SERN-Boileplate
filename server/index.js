8baf5fb95f7a948185ae58760276ea5b959bdc46
TheEnderOfficial <enderofficial.dp@yandex.ru>
2022-10-15 20:43:16 +0300

    Admin, another things

--- server/index.js
+++ server/index.js
@@ -2,11 +2,39 @@ require("dotenv").config()
 const express = require('express');
 const sequelize = require("./db/index");
 const AuthRouter = require("./routers/authRouter")
+const AdminJS = require('adminjs')
+const AdminJSExpress = require('@adminjs/express')
+const AdminJSSequelize = require("@adminjs/sequelize");
 
 const app = express();
+AdminJS.registerAdapter({
+    Resource: AdminJSSequelize.Resource,
+    Database: AdminJSSequelize.Database,
+})
+
+const admin = new AdminJS({
+    resources: [
+        {
+            resource: sequelize.User,
+            options: {
+                listProperties: ["id", "username", "family_name"],
+                showProperties: ["id", "username", "family_name"],
+                editProperties: ["id", "username", "family_name"],
+                filterProperties: ["id", "username", "family_name"],
+            }
+        }
+    ],
+    branding: {
+        companyName: 'Hackathon',
+        logo: "https://avatars.githubusercontent.com/u/80571676?s=75&v=4",
+        softwareBrothers: false
+    },
+})
+const adminRouter = AdminJSExpress.buildRouter(admin)
 
 app.use(require("cors")())
 app.use(express.json());
+app.use(admin.options.rootPath, adminRouter)
 app.use("/auth/", AuthRouter);
 
 const main = async () => {
